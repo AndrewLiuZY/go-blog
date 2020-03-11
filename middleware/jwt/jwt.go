09b9e3a357ee093d20c2b97544a224513c6e2e66
AndrewLiu <317369453@qq.com>
2020-03-11 11:01:36 +0800

    Update jwt.go

--- middleware/jwt/jwt.go
+++ middleware/jwt/jwt.go
@@ -6,8 +6,8 @@ import (
 
 	"github.com/gin-gonic/gin"
 
-	"github.com/andrewliuzy/gin-blog/pkg/e"
-	"github.com/andrewliuzy/gin-blog/pkg/util"
+	"gin-blog/pkg/e"
+	"gin-blog/pkg/util"
 )
 
 func JWT() gin.HandlerFunc {
